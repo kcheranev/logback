1f9064d4a73b4bc803144a03d77c8e89a7021926
kcheranev <kcheranev@alfabank.ru>
2019-02-11 18:07:38 +0300

    fix LayoutWrappingEncoder

--- logback-core/src/main/java/ch/qos/logback/core/encoder/LayoutWrappingEncoder.java
+++ logback-core/src/main/java/ch/qos/logback/core/encoder/LayoutWrappingEncoder.java
@@ -1,24 +1,24 @@
 /**
  * Logback: the reliable, generic, fast and flexible logging framework.
  * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
- *
+ * <p>
  * This program and the accompanying materials are dual-licensed under
  * either the terms of the Eclipse Public License v1.0 as published by
  * the Eclipse Foundation
- *
- *   or (per the licensee's choosing)
- *
+ * <p>
+ * or (per the licensee's choosing)
+ * <p>
  * under the terms of the GNU Lesser General Public License version 2.1
  * as published by the Free Software Foundation.
  */
 package ch.qos.logback.core.encoder;
 
-import java.nio.charset.Charset;
-
-import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.CoreConstants;
 import ch.qos.logback.core.Layout;
 import ch.qos.logback.core.OutputStreamAppender;
+import ch.qos.logback.core.spi.ContextAware;
+
+import java.nio.charset.Charset;
 
 public class LayoutWrappingEncoder<E> extends EncoderBase<E> {
 
@@ -33,8 +33,8 @@ public class LayoutWrappingEncoder<E> extends EncoderBase<E> {
      */
     private Charset charset;
 
-    Appender<?> parent;
-    Boolean immediateFlush = null;
+    private ContextAware parent;
+    private Boolean immediateFlush;
 
     public Layout<E> getLayout() {
         return layout;
@@ -145,12 +145,11 @@ public class LayoutWrappingEncoder<E> extends EncoderBase<E> {
     }
 
     /**
-     * This method allows RollingPolicy implementations to be aware of their
-     * containing appender.
-     * 
-     * @param appender
+     * This method allows RollingPolicy implementations to be aware of their parent.
+     *
+     * @param parent
      */
-    public void setParent(Appender<?> parent) {
+    public void setParent(ContextAware parent) {
         this.parent = parent;
     }
 }
